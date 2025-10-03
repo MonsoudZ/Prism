ce7f780b3d13d5ad4ed8762a92ff375b8c605e88
Monsoud Zanaty <227215732+MonsoudZanaty@users.noreply.github.com>
2025-10-03 15:28:14 -0400

    Initial Commit

---
+++ Prism/PrismApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrismApp.swift
+//  Prism
+//
+//  Created by Monsoud Zanaty on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PrismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
